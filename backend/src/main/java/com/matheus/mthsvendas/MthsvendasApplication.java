ff521333d535d7d75ff76a7cd7159c4bd1602a68
Matheus Alan <matheusalanpv@outlook.com>
2021-09-06 23:48:35 -0300

    Project created

---
+++ backend/src/main/java/com/matheus/mthsvendas/MthsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.mthsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MthsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MthsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/matheus/mthsvendas/MthsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.mthsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MthsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
