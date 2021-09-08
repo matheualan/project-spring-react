c3020b2900d213c7997f8fac484b90d47b66b76e
Matheus Alan <matheusalanpv@outlook.com>
2021-09-08 12:52:43 -0300

    security config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Total Vendas</h5>
+            <h5 className="text-center text-secondary">Total de Vendas</h5>
             <DonutChart />
           </div>
         </div>
